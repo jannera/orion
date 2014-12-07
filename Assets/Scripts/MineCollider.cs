d0a0ad3832c38626f1bb92431f56f53e7fc2ed2a
gilead <janne.rasanen@iki.fi>
2014-12-07 18:06:55 +0200

    Added explosion sound, particle effect and light effect when a mine goes off

--- Assets/Scripts/MineCollider.cs
+++ Assets/Scripts/MineCollider.cs
@@ -2,10 +2,14 @@
 using System.Collections;
 
 public class MineCollider : MonoBehaviour {
-
+    private AudioSource audio;
+    private ParticleSystem particle;
+    private Animator anim;
 	// Use this for initialization
 	void Start () {
-	
+        audio = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+        particle = GetComponent<ParticleSystem>();
 	}
 	
 	// Update is called once per frame
@@ -16,7 +20,13 @@ public class MineCollider : MonoBehaviour {
     void OnTriggerEnter(Collider other)
     {
         other.gameObject.GetComponent<OrionExploder>().Explode(transform.position);
+        foreach (Transform t in transform)
+        {
+            t.gameObject.SetActive(false);
+        }
+        audio.Play();
+        particle.Play();
+        anim.SetTrigger("Explode");
         Destroy(other.gameObject);
-        Destroy(this.gameObject);
     }
 }
