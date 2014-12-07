7692af3d0e5e8be39b699a3eb77588f2f26b7d8b
gilead <janne.rasanen@iki.fi>
2014-12-07 11:19:35 +0200

    Added rotating mines. Planet now rotates. Started building level selection canvas.

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotation : MonoBehaviour {
+    public float maxSpin = 1f;
+
+	void Start () {
+        Vector3 spin = new Vector3(Random.Range(-maxSpin, maxSpin), Random.Range(-maxSpin, maxSpin), Random.Range(-maxSpin, maxSpin));
+        spin *= rigidbody.mass;
+        rigidbody.AddTorque(spin, ForceMode.Impulse);
+	}
+}
---
+++ Assets/Scripts/SteadyRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteadyRotation : MonoBehaviour {
+    public Vector3 spin;
+	
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 diff = -(rigidbody.angularVelocity - spin);
+        rigidbody.AddTorque(diff * Time.deltaTime, ForceMode.VelocityChange);
+    }
+}
